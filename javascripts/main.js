0ff8ed8f9b0ac7e048d2b322291080e50ef2cf89
calbearox <99705133+calbearox@users.noreply.github.com>
2022-02-21 16:36:43 -0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,2 @@
-console.log('This would be the main JS file.');
+${maldito-dot}foo +-- node_modules +-- blerg (1.2.5) <---[A] +-- bar (1.2.3) <---[B] | `-- node_modules | +-- baz (2.0.2) <---[C] | | `-- node_modules | | `-- quux (3.2.0) | `-- asdf (2.3.4) `-- baz (1.2.3) <---[D] `-- node_modules `-- quux (3.2.0) <---[E]
+
