b737e9461114cf1c237367ea8d4f5e441739c402
calbearox <99705133+calbearox@users.noreply.github.com>
2022-03-13 21:23:46 -0700

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,6 @@
-${maldito-dot}foo +-- node_modules +-- blerg (1.2.5) <---[A] +-- bar (1.2.3) <---[B] | `-- node_modules | +-- baz (2.0.2) <---[C] | | `-- node_modules | | `-- quux (3.2.0) | `-- asdf (2.3.4) `-- baz (1.2.3) <---[D] `-- node_modules `-- quux (3.2.0) <---[E]
-
+${maldito-dot}node+-- node_modules +-- blerg (1.2.5) <---[A] +-- bar (1.2.3) <---[B] | `-- node_modules | +-- baz (2.0.2) <---[C] | | `-- node_modules | | `-- quux (3.2.0) | `-- asdf (2.3.4) `-- baz (1.2.3) <---[D] `-- node_modules `-- quux (3.2.0) <---[E]
+fun main() {
+   val kotlin = "🙂"
+   println(kotlin)
+}
+//.maldito-dot/readme-md
